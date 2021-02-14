b0a48e2bd024a055148a88a35a02db80bc15104d
Dustin Sugasa <dsugasa@lmr.corp>
2021-02-14 11:57:06 +0000

    adding dropbox file

---
+++ upload.py
@@ -0,0 +1,20 @@
+import dropbox
+
+access_token = 'XXXX'
+file_from = '2021_1_26_19_1611687610393.jpg'  # //local file path
+file_to = '/Upload_Tester_Checker/2021_1_26_19_1611687610393.jpg'  # // dropbox path
+
+
+class DboxUploader:
+    def __init__(self, access_token, file_from, file_to):
+        self.access_token = access_token
+        self.file_from = file_from
+        self.file_to = file_to
+
+    def upload_file(self):
+        dbx = dropbox.Dropbox(self.access_token)
+        f = open(self.file_from, 'rb')
+        dbx.files_upload(f.read(), self.file_to)
+
+
+#DboxUploader(access_token,file_from, file_to).upload_file()
